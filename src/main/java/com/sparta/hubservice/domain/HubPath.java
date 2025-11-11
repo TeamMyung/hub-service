11bdb253f058796bdcab2e09be25712b85350eef
SeongYeon Han <cicle1999@gmail.com>
2025-11-11 17:27:53 +0900

    add(hub-path): HubPath 엔티티 생성

---
+++ src/main/java/com/sparta/hubservice/domain/HubPath.java
@@ -0,0 +1,38 @@
+package com.sparta.hubservice.domain;
+
+import com.sparta.hubservice.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "p_hub_paths")
+public class HubPath extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "hub_path_id")
+    private UUID hubPathId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "start_hub_id", nullable = false)
+    private Hub startHub;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "end_hub_id", nullable = false)
+    private Hub endHub;
+
+    @Column(nullable = false)
+    private Integer duration;
+
+    @Column(nullable = false)
+    private Integer distance;
+}
